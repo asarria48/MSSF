203d866783a4d60f0a07239ba2735812784ea3c6
asarria48 <92031732+asarria48@users.noreply.github.com>
2024-02-14 01:24:06 +0000

    Método de bisección en C++

--- C1_MSF.cpp
+++ C1_MSF.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 #include <cmath>
-
 using namespace std;
+
+const double error = 1e-7;
+
+double f(double x){
+    return sin(x)/x;
+}
 
 int main(){
 
-    cout << "Adiós mundo" << endl;
+double a = 2, b = 4, m, fa, fm;
+fa = f(a);
+
+while(b-a>error){
+    m = (b+a)/2;
+    fm = f(m);
+
+    if(fa*fm > 0){
+    fa = fm;
+    a = m;
+    }
+
+    else
+    b = m;
+}
+
+    cout << "El cero es x = " << (b+a)/2 << endl;
 
     return 0;
 }
